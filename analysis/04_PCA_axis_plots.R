b47f41dbfdee8d63aae914a982bff19f75652844
Selin Kubilay <d.s.kubilay@tudelft.nl>
2024-07-22 22:17:30 +0200

    changed the color of the pca boxplots according to the new grouping

--- analysis/04_PCA_axis_plots.R
+++ analysis/04_PCA_axis_plots.R
@@ -14,22 +14,19 @@ pl = ggplot(data = Multi_centroid, aes(x = groups, y = disp_PC_all)) +
 
 
 ##### Figure 6B -  PC1 Dispersion #####
-PCA_results$alteration_type_grouping = factor(PCA_results$alteration_type_grouping, 
-                              levels = c("TempNat", "TempAlt_irrigation", "TempAlt_hydropower", "MedNat", "MedAlt_irrigation"))
-
 individual_pc1 = ggplot(data = PCA_results, aes(x = groups.x, y = PC1)) + 
-  geom_boxplot(aes(x = groups.x, y = PC1,  fill = alteration_type_grouping, group = reorder(site, PC1, median)),  lwd = 0.2, outlier.size = 0.5) +
+  geom_boxplot(aes(x = groups.x, y = PC1,  fill = alteration_type_grouping, group = reorder(site, PC1, median)), lwd = 0.2, outlier.size = 0.5) +
   scale_y_continuous(limits = c(-5, 7.5)) +
-  scale_fill_manual(values =c("#B4DCED", '#6996D1', "#2B5FA2", '#F5CB7D','#F09E41'))+
+  scale_fill_manual(values =c("#B4DCED", '#6996D1', "#2B5FA2", '#F5CB7D', '#F09E41'))+
   scale_x_discrete(labels = c("MedAlt" = "aM","MedNat" = "nM", "TempAlt" = "aA", "TempNat" = "nA")) +
-  theme_pca() + theme(legend.position = "top")
+  theme_pca() + theme(legend.position = "bottom")
   ylab("PC1") +
   theme_boxplot()
 
 ##### Figure 6C -  PC1 Dispersion #####
 individual_pc2 = ggplot(data = PCA_results, aes(x = groups.x, y = PC2)) + 
-  geom_boxplot(aes(x = groups.x, y = PC2, fill = groups.x, group = reorder(site, PC2, median)),lwd = 0.2, outlier.size = 0.5)+
-  scale_fill_manual(values = c("#B4DCED", '#6996D1','#F5CB7D','#F09E41'))+
+  geom_boxplot(aes(x = groups.x, y = PC2, fill = alteration_type_grouping, group = reorder(site, PC2, median)), lwd = 0.2, outlier.size = 0.5)+
+  scale_fill_manual(values = c("#B4DCED", '#6996D1', "#2B5FA2", '#F5CB7D', '#F09E41'))+
   scale_x_discrete(labels = c("MedAlt" = "aM","MedNat" = "nM", "TempAlt" = "aA", "TempNat" = "nA"))+
   scale_shape_manual(values = c(2.5,7.5))+
   theme_pca() +
